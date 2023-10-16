13706b2c24d039d0f6e7626c8b3010aed7e634d7
tiyn <tiyn@mail-mk.eu>
2023-10-16 18:40:52 +0200

    made volume check every other minute

--- config.c
+++ config.c
@@ -4,13 +4,13 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"memory",       10,   6},
-    {"cpu intel",          10,   5},
-    {"disk /",       1800, 7},
-    {"volume",       0,    4},
-    {"battery BAT0", 5,    2},
-    {"internet",     5,    3},
-    {"clock",        60,   1},
+    {"memory",        10,   6},
+    {"cpu amd",       10,   5},
+    {"disk /home",    1800, 7},
+    {"volume",        120,  4},
+    {"battery BAT0",  5,    2},
+    {"internet",      5,    3},
+    {"clock",         60,   1},
 };
 
 const unsigned short blockCount = LEN(blocks);
